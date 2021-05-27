1bf4444e54640f0a15fe89fd6210e1794897765a
Unknown <czhan7@ocdsb.ca>
2021-05-27 12:32:55 -0400

    renaming

--- main.cpp
+++ main.cpp
@@ -5,39 +5,39 @@
 #include <iostream>
 using namespace std;
 
-//void baseIngredientTest()
-//{
-//    BaseIngredient b1("Life", 0);
-//    BaseIngredient b1("Death", 1);
-//    BaseIngredient b1("Wood", 2);
-//    BaseIngredient b1("Water", 3);
-//    BaseIngredient b1("Fire", 4);
-//    BaseIngredient b1("Love", 5);
-//    BaseIngredient b1("Electricity", 6);
-//    BaseIngredient b1("Air", 7);
-//    BaseIngredient b1("Cupcake", 8);
-//    BaseIngredient b1("Toothbrush", 9);
-//}
+void baseIngredientTest()
+{
+    BaseIngredient b0("Life", 0);
+    BaseIngredient b1("Death", 1);
+    BaseIngredient b2("Wood", 2);
+    BaseIngredient b3("Water", 3);
+    BaseIngredient b4("Fire", 4);
+    BaseIngredient b5("Love", 5);
+    BaseIngredient b6("Electricity", 6);
+    BaseIngredient b7("Air", 7);
+    BaseIngredient b8("Cupcake", 8);
+    BaseIngredient b9("Toothbrush", 9);
+}
 
-//void potionTest() {
-//    Potion p1(0, 1, "testinggg", false);
-//    Potion p2(1, 2, "testinggg2", false);
-//    p1.setBaseIngredient1(3);
-//    p1.setBaseIngredient1(90);
-//    cout << p1.toString() << endl;
-//    cout << p2.toString() << endl;
-//}
+void potionTest() {
+    Potion p1(0, 1, "testinggg", false);
+    Potion p2(1, 2, "testinggg2", false);
+    p1.setBaseIngredient1(3);
+    p1.setBaseIngredient1(90);
+    cout << p1.toString() << endl;
+    cout << p2.toString() << endl;
+}
 
 int main() {
- //   Customer c1;
-//    Customer c2("Dude", "613-123-4566", 2, true);
-//    Customer c3("Name", "1234567-8999", 10, false);
-//    cout << c1.toString() << endl;
-//    cout << c2.toString() << endl;
-//    cout << c3.toString() << endl;
-//    string ph = "111-111-1111";
-//    c3.setPhone(ph);
-//    cout << c3.getPhone() << endl;
+    Customer c1;
+    Customer c2("Dude", "613-123-4566", 2, true);
+    Customer c3("Name", "1234567-8999", 10, false);
+    cout << c1.toString() << endl;
+    cout << c2.toString() << endl;
+    cout << c3.toString() << endl;
+    string ph = "111-111-1111";
+    c3.setPhone(ph);
+    cout << c3.getPhone() << endl;
     cout << "hello" << endl;
     
     return 0;
